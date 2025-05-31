dfb6670d1187e1638077681799c9f6cc2ef38a17
EdoardoPosfortunato <edoardo.posfortunato27@gmail.com>
2025-05-31 12:22:20 +0200

    feat: footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,68 @@
+import style from "./Footer.module.css"
+
 function Footer() {
   return (
     <>
-    <h1>Footer</h1>
+      <div className={style.footer}>
+        <div className={style.container}>
+          <div className={style.totalList}>
+            <div className={style.flexList}>
+              <h2>DC COMICS</h2>
+              <ul>
+                <li>Characters</li>
+                <li>Comics</li>
+                <li>Lorem</li>
+                <li>Lorem</li>
+                <li>Lorem</li>
+                <li>Lorem</li>
+                <li>Lorem</li>
+              </ul>
+            </div>
+            <div className={style.flexList}>
+              <h2>DC COMICS</h2>
+              <ul>
+                <li>Characters</li>
+                <li>Comics</li>
+                <li>Lorem</li>
+              </ul>
+            </div>
+            <div className={style.flexList}>
+              <h2>DC COMICS</h2>
+              <ul>
+                <li>Characters</li>
+                <li>Comics</li>
+                <li>Lorem</li>
+                <li>Lorem</li>
+                <li>Lorem</li>
+                <li>Lorem</li>
+                <li>Lorem</li>
+                <li>Lorem</li>
+                <li>Lorem</li>
+                <li>Lorem</li>
+              </ul>
+            </div>
+            <div className={style.flexList}>
+              <h2>DC COMICS</h2>
+              <ul>
+                <li>Characters</li>
+                <li>Comics</li>
+                <li>Lorem</li>
+                <li>Lorem</li>
+                <li>Lorem</li>
+                <li>Lorem</li>
+                <li>Lorem</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={style.signNow}>
+        <div className={style.container}>
+
+        <h1>sign now</h1>
+        </div>
+      </div>
+      div
     </>
   )
 }
